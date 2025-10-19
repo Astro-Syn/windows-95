4b077be3ed47c62ab555960f82309df692f79466
Astro-Syn <kelseyamyb@gmail.com>
2025-10-19 16:03:22 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import Footer from './components/footer/Footer'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      Hello
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/footer/start_menu/StartMenu.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './StartMenu.css';
+
+export default function StartMenu() {
+  return (
+    <div>StartMenu</div>
+  )
+}
